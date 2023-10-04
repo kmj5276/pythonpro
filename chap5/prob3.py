41ff1cb9a8b4a336d4a77c4fc41c6a05090e0e36
kmj5276 <myeongjae1235@gmail.com>
2023-10-04 02:58:12 +0000

    prob3

---
+++ chap5/prob3.py
@@ -0,0 +1,10 @@
+import random
+print("Guess the Word!!!")
+print("In this game, the program selects a word at random, and the player's objective is to guess the chosen word.")
+words = ('difficult', 'banana', 'apple', 'python', 'daegu', 'catholic', 'university')
+word = random.choice(words)
+length = len(word)
+print("Length of the selected word:", length)
+ra = int(6)
+for 
+
