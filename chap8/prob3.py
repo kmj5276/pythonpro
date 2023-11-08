1dc4c4b69b16b206a852dd67da32543b5a831449
kmj5276 <39117186+kmj5276@users.noreply.github.com>
2023-11-08 20:34:09 +0900

    prob3

--- chap8/prob3.py
+++ chap8/prob3.py
@@ -1,1 +1,25 @@
+print("Input your string...")
 
+def Open(file):
+    print("\n\n\nYour inputs are below..")
+    text_file = open(file,"r")
+    whole_thing = text_file.read()
+    print(whole_thing)
+
+def write(file, string):
+    text_file = open(file, "a")
+    text_file.write(string + "\n")
+    text_file.close()
+
+file = "text.txt"
+open(file, "w").close()
+
+while True:
+    string = input(">> ")
+    if string.upper() == 'Q':
+        print("Write process has been finished")
+        break
+    else:
+        write(file, string)
+
+Open(file)
