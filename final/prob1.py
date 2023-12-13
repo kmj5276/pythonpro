d769520381329243611007ba79464fde72d0b9b9
kmj5276 <39117186+kmj5276@users.noreply.github.com>
2023-12-13 10:15:57 +0900

    prob1.py

--- final/prob1.py
+++ final/prob1.py
@@ -1,1 +1,12 @@
+def frequency_analytic(s):
+    dic = {}
+    for i in s:
+        if i in dic:
+            dic[i] += 1
+        else :
+            dic[i]= 1
+    print(dic)
 
+if __name__ == "__main__":
+    s = input('input your message : ')
+    frequency_analytic(s)
